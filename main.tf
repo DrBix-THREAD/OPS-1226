3c4426b460ca8e4fa3e0481c3303179cf2aea2de
DrBix-THREAD <164527379+DrBix-THREAD@users.noreply.github.com>
2024-03-27 14:14:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,17 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "bix-bucket"
   acl    = "private"
 }
+
+resource "aws_sns_topic" "bucket_notifications" {
+  name = "bucket-notifications"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  topic {
+    topic_arn     = aws_sns_topic.bucket_notifications.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "logs/"
+  }
+}
